85709b3e7250ebcd3c95dd50a06d4d3ca7557311
JorenSix <joren.six@hogent.be>
2012-02-17 14:44:12 +0100

    Added package level info for filters package.

---
+++ src/be/hogent/tarsos/dsp/filters/package-info.java
@@ -0,0 +1,14 @@
+/**
+*
+*  TarsosDSP is developed by Joren Six at 
+*  The Royal Academy of Fine Arts & Royal Conservatory,
+*  University College Ghent,
+*  Hoogpoort 64, 9000 Ghent - Belgium
+*  
+*  http://tarsos.0110.be/tag/TarsosDSP
+*
+**/
+/**
+ * This package contains a couple of filters (low and highpass). 
+ */
+package be.hogent.tarsos.dsp.filters;
