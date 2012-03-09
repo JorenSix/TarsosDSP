5801dd0ae73a2c43e210f4c9b621024f8835f5f6
JorenSix <joren.six@hogent.be>
2012-03-09 10:12:11 +0100

    Now correctly updated the header message...

--- examples/be/hogent/tarsos/dsp/example/PitchDetector.java
+++ examples/be/hogent/tarsos/dsp/example/PitchDetector.java
@@ -8,16 +8,7 @@
 *  http://tarsos.0110.be/tag/TarsosDSP
 *
 */
-/**
-*
-*  TarsosDSP is developed by Joren Six at 
-*  The Royal Academy of Fine Arts & Royal Conservatory,
-*  University College Ghent,
-*  Hoogpoort 64, 9000 Ghent - Belgium
-*  
-*  http://tarsos.0110.be/tag/TarsosDSP
-*
-**/
+
 package be.hogent.tarsos.dsp.example;
 
 import java.awt.GridLayout;
