3d3ac98fd5a0d10934627f735e74e7c7ce0e7e33
JorenSix <joren.six@hogent.be>
2012-12-20 15:09:49 +0100

    Added an optional median filter.

--- src/be/hogent/tarsos/dsp/synthesis/PitchResyntheziser.java
+++ src/be/hogent/tarsos/dsp/synthesis/PitchResyntheziser.java
@@ -1,5 +1,7 @@
 package be.hogent.tarsos.dsp.synthesis;
 
+import java.util.Arrays;
+
 import be.hogent.tarsos.dsp.AudioEvent;
 import be.hogent.tarsos.dsp.EnvelopeFollower;
 import be.hogent.tarsos.dsp.pitch.PitchDetectionHandler;
@@ -35,27 +37,51 @@ public class PitchResyntheziser implements PitchDetectionHandler {
 	private final EnvelopeFollower envelopeFollower;
 	private boolean usePureSine;
 	private boolean followEnvelope;
+	private final double[] previousFrequencies;
+	private int previousFrequencyIndex;
 	
 	public PitchResyntheziser(float samplerate){
 		this(samplerate,true,false);
 	}
 	
 	public PitchResyntheziser(float samplerate,boolean followEnvelope,boolean pureSine){
+		this(samplerate,followEnvelope,pureSine,5);
+	}
+	
+	public PitchResyntheziser(float samplerate,boolean followEnvelope,boolean pureSine,int filterSize){
 		envelopeFollower = new EnvelopeFollower(samplerate,0.005,0.01);
 		this.followEnvelope=followEnvelope;
 		this.usePureSine = pureSine;
 		this.samplerate = samplerate;
+		previousFrequencies = new double[filterSize];
+		previousFrequencyIndex = 0;
 	}
 	
 	@Override
 	public void handlePitch(PitchDetectionResult pitchDetectionResult,
 			AudioEvent audioEvent) {
 		double frequency = pitchDetectionResult.getPitch();
+		
 		if(frequency==-1){
 			frequency=prevFrequency;
 		}else{
+			if(previousFrequencies.length!=0){
+				//median filter
+				//store and adjust pointer
+				previousFrequencies[previousFrequencyIndex] = frequency;
+				previousFrequencyIndex++;
+				previousFrequencyIndex %= previousFrequencies.length;
+				//sort to get median frequency
+				double[] frequenciesCopy = previousFrequencies.clone();
+				Arrays.sort(frequenciesCopy);
+				//use the median as frequency
+				frequency = frequenciesCopy[frequenciesCopy.length/2];
+			}
+			
 			prevFrequency = frequency;
 		}
+		
+		
 	
 		final double twoPiF = 2 * Math.PI * frequency;
 		float[] audioBuffer = audioEvent.getFloatBuffer();
