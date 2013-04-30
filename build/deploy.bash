604d5619aa0bd7da39f465cb5f88eb28d4794730
JorenSix <joren.six@hogent.be>
2013-04-30 12:17:15 +0200

    Updated build scripts to reflect source code location changes

--- build/deploy.bash
+++ build/deploy.bash
@@ -6,6 +6,13 @@ manual_location="../../TarsosTex/sound_and_java.pdf"
 #Remove old releases
 rm -R TarsosDSP-*
 
+#link the source files at the expected locations
+cd ..
+test -f src || ln -s TarsosDSP/src/main/java/ src
+test -f examples || ln -s TarsosDSPExamples/src/main/java/ examples
+test -f tests || ln -s TarsosDSP/src/test/java tests 
+cd build
+
 #Build the new release
 ant release
 
--- build/get_latest_sources.bash
+++ build/get_latest_sources.bash
@@ -9,6 +9,9 @@
 wget https://github.com/JorenSix/TarsosDSP/zipball/master/ -O TarsosDSP.zip
 unzip TarsosDSP.zip
 cd JorenSix-TarsosDSP*/build/
+ln -s TarsosDSPExamples/src/main/java/ examples
+ln -s TarsosDSP/src/main/java/ src
+ln -s TarsosDSP/src/test/java tests
 bash deploy_nightly.bash
 cd ..
 cd ..
