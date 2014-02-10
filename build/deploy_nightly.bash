4ecc540c6c142145839b5d1296a461082363a8fc
Joren Six <joren.six@ugent.be>
2014-02-10 10:33:12 +0100

    Updated nightly deploy script

--- build/deploy_nightly.bash
+++ build/deploy_nightly.bash
@@ -4,13 +4,6 @@ manual_location="TarsosDSP-latest-Manual.pdf"
 #Remove old releases
 rm -R TarsosDSP-*
 
-#link the source files at the expected locations
-cd ..
-#test -f src || ln -s TarsosDSP/src/main/java/ src
-#test -f examples || ln -s TarsosDSPExamples/src/main/java/ examples
-#test -f tests || ln -s TarsosDSP/src/test/java tests 
-cd build
-
 #Build the new release
 ant release
 
@@ -32,17 +25,11 @@ mv $manual_location TarsosDSP-$version-Manual.pdf
 #Remove old nightly version from the server:
 ssh joren@0110.be rm -R $deploy_location
 
+#create a new dir
 ssh joren@0110.be mkdir $deploy_location
 
 #Deploy to the server 
 scp -r TarsosDSP-* joren@0110.be:$deploy_location
-
-#Find the current version
-filename=$(basename TarsosDSP-*-bin.jar)
-version=${filename:10:3}
-
-deploy_dir="/var/www/be.0110/current/public/releases/TarsosDSP/"
-deploy_location=$deploy_dir"TarsosDSP-nightly"
 
 ssh joren@0110.be mv $deploy_location/TarsosDSP-$version.jar $deploy_location/TarsosDSP-nightly.jar
 ssh joren@0110.be mv $deploy_location/TarsosDSP-$version-bin.jar $deploy_location/TarsosDSP-nightly-bin.jar
